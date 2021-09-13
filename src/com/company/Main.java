6e7843a83538437519c3b26384f701e21769bd3d
Tobias Winkel <winkeltobias@gmail.com>
2021-09-13 08:42:08 +0200

    1st commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int year=in;
+        isLeapyear=boolean;
+        if (isLeapYear = true) ;
+        {
+            System.out.println("Skudår");
+        } else if(isLeapYear = false) {
+            System.out.println("Ikke skudår");
+        } else {
+            System.out.println("Fejl");
+        }
+        public static boolean isLeapYear(int year){
+            if (year % 4 == 0) && (year % 100 != 0) && (year % 400 == 0) {
+                isLeapYear = true;
+            } else {
+                isLeapYear = false;
+            }
+            }
+        }
+    }
